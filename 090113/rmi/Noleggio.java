47611c4cde1398ade4be1101145f6641d4678833
Luca Bartolomei <bartn8@hotmail.it>
2019-12-17 14:25:51 +0100

    First Upload

---
+++ 090113/rmi/Noleggio.java
@@ -0,0 +1,18 @@
+package rmi;
+
+public class Noleggio{
+
+    int id;
+    int giorno;
+    int mese;
+    int anno;
+    int giorni;
+    String modello;
+    int costoGiornaliero;
+
+
+    public Noleggio(int id, int giorno, int mese, int anno){
+
+    }
+
+}
